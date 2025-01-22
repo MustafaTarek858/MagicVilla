83a80e44e3cab68e370a43872b7f2e615f3b97e8
Moustafa.tarek <Moustafa.tarek@MOSTAFA-TAREK>
2025-01-22 10:58:22 +0200

    - added model - added Data folder - added get method to the base controller

---
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        //[Route("api/[controller]")]
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+    }
+}
