8feae3aaab8b1f418cb24d3a64ec11c79355b959
Moustafa.tarek <Moustafa.tarek@MOSTAFA-TAREK>
2025-01-26 05:39:05 +0200

    adjusting controller

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -11,9 +11,52 @@ namespace MagicVilla_VillaApi.Controllers
     {
         //[Route("api/[controller]")]
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
+        
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(200,Type=typeof(VillaDTO))]
+
+        public ActionResult<VillaDTO> GetVilla(int id) 
+        {
+            if (id == 0) 
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO) 
+        {
+            if (villaDTO == null) 
+            { 
+                return BadRequest(villaDTO);
+            }
+            if(villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+
+            VillaStore.villaList.Add(villaDTO);
+
+            return Ok(villaDTO);
+        
+        }
+
     }
 }
